798d2c3a8671790147f4d8f595774d1a14708e6a
prashantpalinfinitelocus <prashant.pal@infintelocus.com>
2025-12-06 16:08:51 +0530

    fixed the db

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,28 +1,21 @@
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections = {
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'thums-up'),
-        user: env('DATABASE_USERNAME', 'prashantpal'),
-        password: env('DATABASE_PASSWORD', 'password123'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-  };
-
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'thums-up'),
+      user: env('DATABASE_USERNAME', 'prashantpal'),
+      password: env('DATABASE_PASSWORD', 'password123'),
+      ssl: env.bool('DATABASE_SSL', false) && {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+      },
+      schema: env('DATABASE_SCHEMA', 'public'),
     },
-  };
-};
+    pool: { 
+      min: env.int('DATABASE_POOL_MIN', 2), 
+      max: env.int('DATABASE_POOL_MAX', 10) 
+    },
+    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+  },
+});
