299deffd604c4c8b72993ea3d2797bb6ae189691
prashantpalinfinitelocus <prashant.pal@infintelocus.com>
2025-12-06 13:38:50 +0530

    Fixed the strapi

--- migrations/002_add_name_to_notify_me.sql
+++
@@ -1,6 +0,0 @@
--- Add name column to notify_me table
-ALTER TABLE notify_me ADD COLUMN IF NOT EXISTS name VARCHAR(255) NOT NULL DEFAULT '';
-
--- Update the default constraint to allow proper insertion
-ALTER TABLE notify_me ALTER COLUMN name DROP DEFAULT;
-
