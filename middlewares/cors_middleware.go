5cedca350e1ccca5b1e80c76a5e61905c097e291
prashantpalinfinitelocus <prashant.pal@infintelocus.com>
2025-12-12 14:09:53 +0530

    Fixed the cors issue

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -6,7 +6,6 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// cfg := config.GetConfig()
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-API-Key")
